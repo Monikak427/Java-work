85cd7374a00d822f2c3cd7082ef42ad8bf5b8302
monikak427 <monika.sharmak427@gmail.com>
2024-01-11 10:18:36 -0600

    New file is added

--- Student.java
+++ Student.java
@@ -1,10 +1,8 @@
 public class Student {
     private String name = "Monika";
-
     public String getName() {
         return name;
     }
-
     public static void main(String[] args) {
         Student s = new Student();
         System.out.println("Name:"+s.getName());
