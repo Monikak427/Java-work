24dd8040b3277f6f85866a6dd21ab9e86cb030dd
monikak427 <monika.sharmak427@gmail.com>
2024-01-17 10:43:27 -0600

    Files added

---
+++ src/Reverse.java
@@ -0,0 +1,15 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num= 12345;
+		int rev = 0;
+		while(num!=0) {
+			int temp = num%10;
+			rev = rev * 10 + temp;
+			num = num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
